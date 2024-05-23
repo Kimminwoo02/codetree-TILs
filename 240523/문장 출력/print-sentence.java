68e6f239fc625abb6780d14e4e7e71a10bebe342
Minutaurus <71600656+Kimminwoo02@users.noreply.github.com>
2024-05-23 21:10:15 +0900

    Title: 문장 출력, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240523/문장 출력/print-sentence.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("Hello World!");
+        bw.flush();
+    }
+}
