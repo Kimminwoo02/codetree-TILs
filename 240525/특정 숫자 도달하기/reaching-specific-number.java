e8d73ed706af6b598f154779a79638297fe5fecf
Minutaurus <71600656+Kimminwoo02@users.noreply.github.com>
2024-05-25 12:45:23 +0900

    Title: 특정 숫자 도달하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240525/특정 숫자 도달하기/reaching-specific-number.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Integer total = 0;
+        Double avg = 0.0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while(true){
+            String data = st.nextToken();
+            Integer res = Integer.parseInt(data);
+            if( res >= 250){
+                break;
+            }
+            else{
+                total += res;
+                avg += 1.0;
+            }
+            
+        }
+
+        bw.write(total +" " + Math.round(total/avg * 10) / 10.0);
+        bw.flush();
+
+        bw.close();
+        br.close();    
+    }
+}
