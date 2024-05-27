75a267816c0935e65df30a7565795dc4f8a4a2c2
Minutaurus <71600656+Kimminwoo02@users.noreply.github.com>
2024-05-27 23:20:34 +0900

    Title: 미는 횟수, Time: 104ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240527/미는 횟수/number-of-pushes.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String arr1 = br.readLine();
+        String arr2 = br.readLine();
+        String res = arr1;
+        Integer cnt = 1;
+        for (int i =0; i<arr1.length();i++){
+            char data = res.charAt(0);
+            res = res.substring(1) + data;
+            if(!res.equals(arr2)) {
+                 cnt+=1;
+                
+            }
+            else {
+                bw.write(cnt+"");
+                bw.flush();
+                System.exit(0);
+            }
+        }
+
+        bw.write(-1+"");
+        bw.flush();
+    }
+}
