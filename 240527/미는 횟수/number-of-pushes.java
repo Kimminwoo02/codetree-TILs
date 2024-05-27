305fe6f5a6630cb3ba5c675252701483d22a78ea
Minutaurus <71600656+Kimminwoo02@users.noreply.github.com>
2024-05-27 23:23:52 +0900

    Title: 미는 횟수, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240527/미는 횟수/number-of-pushes.java
+++ 240527/미는 횟수/number-of-pushes.java
@@ -8,19 +8,19 @@ public class Main {
         String arr1 = br.readLine();
         String arr2 = br.readLine();
         String res = arr1;
-        Integer cnt = 1;
+        Integer cnt = 0;
         for (int i =0; i<arr1.length();i++){
-            char data = res.charAt(0);
-            res = res.substring(1) + data;
+     
             if(!res.equals(arr2)) {
-                 cnt+=1;
-                
+                 cnt+=1;                
             }
             else {
                 bw.write(cnt+"");
                 bw.flush();
                 System.exit(0);
             }
+            char data = res.charAt(0);
+            res = res.substring(1) + data;
         }
 
         bw.write(-1+"");
